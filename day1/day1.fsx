f11b1db1cc93ecab5144f905be303a7e7f8788da
Heidi Mork <heidimork@yahoo.no>
2022-12-01 19:52:57 +0100

    Day 1

---
+++ day1/day1.fsx
@@ -0,0 +1,24 @@
+open System.IO
+
+let fileToElfCalories () = 
+    File.ReadLines "input.txt"
+    |> Seq.toList
+    |> List.fold (
+        fun (curr, groups) l 
+            -> if l = "" then ([], curr :: groups) else (int l :: curr, groups) ) 
+        ([], [])
+    |> snd
+    |> List.map List.sum
+ 
+
+let calories = fileToElfCalories ()
+let task1 = List.max calories
+let task2 = 
+    calories
+    |> List.sortDescending 
+    |> List.take 3 
+    |> List.sum
+
+
+printfn $"Task 1: {task1}" // 66306
+printfn $"Task 2 {task2}" // 195292
