4f486397d014cf8fd4b327b6f391683b7565246f
Heidi Mork <heidimork@yahoo.no>
2022-12-02 18:10:51 +0100

    tidy up

--- day2/day2.fsx
+++ day2/day2.fsx
@@ -5,36 +5,33 @@ type Outcome = Loose | Draw | Win
 
 exception BadInput of string
 
-let charToHand char =
-    match char with
+let charToHand letter =
+    match letter with
     | 'X' | 'A' -> Rock
     | 'Y' | 'B' -> Paper
     | 'Z' | 'C' -> Scissors
-    | c -> raise <| BadInput $"Bad input {c}"
+    | l -> raise <| BadInput $"Bad input {l}"
 
-let charToOutcome c = 
-    match c with
+let charToOutcome letter = 
+    match letter with
     | 'X' -> Loose
     | 'Y' -> Draw
     | 'Z' -> Win
-    | c -> raise <| BadInput $"Bad input {c}"
+    | l -> raise <| BadInput $"Bad input {l}"
 
-let readFile secondPlayerMapper = 
+let readFile () = 
     File.ReadLines "input.txt"
-    |> Seq.toList
-    |> List.map (fun l -> (charToHand l[0], secondPlayerMapper l[2]))
+    |> Seq.map (fun l -> (l[0], l[2]))
 
-let getHand handOutcome = 
-    let secondHand = 
-        match handOutcome with
-        | (x, Draw) -> x
-        | (Paper, Win) -> Scissors
-        | (Paper, Loose) -> Rock
-        | (Rock, Win) -> Paper
-        | (Rock, Loose) -> Scissors
-        | (Scissors, Win) -> Rock
-        | (Scissors, Loose) -> Paper
-    (fst handOutcome, secondHand)
+let getHand hand outcome = 
+    match (hand, outcome) with
+    | (x, Draw) -> x
+    | (Paper, Win) -> Scissors
+    | (Paper, Loose) -> Rock
+    | (Rock, Win) -> Paper
+    | (Rock, Loose) -> Scissors
+    | (Scissors, Win) -> Rock
+    | (Scissors, Loose) -> Paper
 
 let round (a, b) = 
     match (a, b) with
@@ -54,12 +51,17 @@ let handPoints hand =
 
 let play hands = 
     hands
-    |> List.fold (fun tot hands -> tot + round hands + handPoints (snd hands) ) 0
+    |> Seq.fold (fun tot hands -> tot + round hands + handPoints (snd hands) ) 0
 
-let task1 = readFile charToHand |> play
+let task1 = 
+    readFile ()
+    |> Seq.map (fun (a, b) ->  (charToHand a, charToHand b))
+    |> play
 let task2 = 
-    readFile charToOutcome
-    |> List.map getHand
+    readFile ()
+    |> Seq.map (fun (a, b) -> 
+        let firstHand = charToHand a in
+        (firstHand, getHand firstHand (charToOutcome b)))
     |> play
 
 printfn $"Task 1: {task1}" // 12855
