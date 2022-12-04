c89b429512257602107d0075e0ad1384c5ca8993
Heidi Mork <heidimork@yahoo.no>
2022-12-04 09:22:48 +0100

    Test scripts (#4)
    * test days
    
    * add exitcode
    
    * add tests
    
    * adjust workflow
    
    * adjust workflow

---
+++ test/test.fsx
@@ -0,0 +1,44 @@
+#r "nuget: Expecto"
+
+open System
+open Expecto
+
+let executeProcess (processName: string) (workingDirectory: string) (args: string list) =
+    let psi = new Diagnostics.ProcessStartInfo(processName)
+    args |> List.iter (fun arg -> psi.ArgumentList.Add(arg))
+    psi.WorkingDirectory <- workingDirectory
+    psi.UseShellExecute <- false
+    psi.RedirectStandardOutput <- true
+    psi.RedirectStandardError <- true
+    psi.CreateNoWindow <- true        
+    let proc = Diagnostics.Process.Start(psi) 
+    let output = new Text.StringBuilder()
+    let error = new Text.StringBuilder()
+    proc.OutputDataReceived.Add(fun args -> output.Append(args.Data) |> ignore)
+    proc.ErrorDataReceived.Add(fun args -> error.Append(args.Data) |> ignore)
+    proc.BeginErrorReadLine()
+    proc.BeginOutputReadLine()
+    proc.WaitForExit()
+    proc.ExitCode, output.ToString()
+
+let testDay day task1expected task2expected = 
+    test $"Test day {day}" {
+    let (exitCode, output) = executeProcess "dotnet" $"day{day}" ["fsi"; $"day{day}.fsx"]
+    Expect.equal exitCode 0 "Program runs ok"
+    let stringTrimmed = output.Replace("Task 1:", "").Replace("Task 2:", "").Trim()
+    let taskResults = stringTrimmed.Split(" ")
+    let task1 = int taskResults[0]
+    let task2 = int taskResults[1]
+    Expect.equal task1 task1expected "Task 1 has expected result"
+    Expect.equal task2 task2expected "Task 2 has expected result"
+  }
+
+let tests =
+    testList "Test all days" [
+        testDay 1 66306 195292
+        testDay 2 12855 13726
+        testDay 3 7428 2650
+        testDay 4 567 907
+    ]
+let exitcode = runTestsWithCLIArgs [] [||] tests
+exit exitcode
