587bedfd68cf7b63d6f81eb05da8b14a87fcf356
Heidi Mork <heidi.mork@nrk.no>
2022-12-15 14:22:19 +0100

    tidy up task1

--- day15/day15.fsx
+++ day15/day15.fsx
@@ -1,6 +1,12 @@
 
 open System.IO
 
+type SensorData = {    
+    Sensor: int * int
+    Beacon: int * int
+    Manhattan: int
+}
+
 let parseLine (line: string) = 
     let coords = 
         line
@@ -11,35 +17,37 @@ let parseLine (line: string) =
     let sensor = (int numbers[0], int numbers[1])
     let beacon = (int numbers[2], int numbers[3])
     let manhattan = abs(fst sensor - fst beacon) + abs(snd sensor - snd beacon)
-    (sensor, beacon, manhattan)
+    {
+        Sensor = sensor
+        Beacon = beacon
+        Manhattan = manhattan
+    }
 
-let withinDistance (y: int) sensor manhattan =
-    let (a, b) = sensor
-    let z = manhattan - abs(b - y)
+let withinDistance (y: int) sensorData =
+    let (a, b) = sensorData.Sensor
+    let z = sensorData.Manhattan - abs(b - y)
     (a - z, z + a)
 
 let readFile () = 
-    let items = 
-        File.ReadLines "input.txt"
-        |> Seq.toList
-        |> List.map parseLine
-    
+    File.ReadLines "input.txt"
+    |> Seq.toList
+    |> List.map parseLine
+
+let task1 =
+    let data = readFile ()
     let idx = 2000000
     let points = 
-        items
-        |> List.map (fun (s,  b, m) -> withinDistance idx s m)
+        data
+        |> List.map (withinDistance idx)
         |> List.map (fun (a, b) -> [a .. b] |> Set.ofList)
         |> List.fold (fun acc s -> Set.union acc s) Set.empty
     let beacons = 
-        items
-        |> List.map (fun (s, b, m) -> b)
+        data
+        |> List.map (fun data -> data.Beacon)
         |> List.filter (fun (x, y) -> y = idx && Set.contains x points)
         |> List.distinct
         |> List.length
     (Set.count points) - beacons
-
-let task1 =
-    readFile ()
 let task2 = "task 2"
 
 printfn $"Task 1: {task1}" // 5240818
