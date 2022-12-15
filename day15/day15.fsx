dc7a051c929026e26c7e84998b58e769cd9bad7f
Heidi Mork <heidimork@yahoo.no>
2022-12-15 08:37:45 +0100

    start day 15

--- day15/day15.fsx
+++ day15/day15.fsx
@@ -1,11 +1,32 @@
 
 open System.IO
 
-let readFile () = 
-    File.ReadLines "input.txt"
-    |> Seq.toList
+let parseLine (line: string) = 
+    let coords = 
+        line
+            .Replace("Sensor at x=", "")
+            .Replace(" y=", "")
+            .Replace(": closest beacon is at x=", ",")
+    let numbers = coords.Split(",")
+    let sensor = (int numbers[0], int numbers[1])
+    let beacon = (int numbers[2], int numbers[3])
+    let manhattan = abs(fst sensor - fst beacon) + abs(snd sensor - snd beacon)
+    (sensor, beacon, manhattan)
 
-let task1 = "task 1"
+let withinDistance (y: int) sensor manhattan =
+    let (a, b) = sensor
+    let z = abs(b - y)
+    (a - manhattan + z, manhattan - z - a)
+
+let readFile () = 
+    File.ReadLines "test-input.txt"
+    |> Seq.toList
+    |> List.map parseLine
+    |> List.map (fun (s,  b, m) -> withinDistance 10 s m)
+    |> List.skip 3
+
+let task1 =
+    readFile ()
 let task2 = "task 2"
 
 printfn $"Task 1: {task1}"
