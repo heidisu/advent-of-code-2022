c5e3745a669b681c8cbdbac5c8f3561ee5399db9
Heidi Mork <heidimork@yahoo.no>
2022-12-18 22:25:59 +0100

    day 14

--- day14/day14.fsx
+++ day14/day14.fsx
@@ -1,11 +1,34 @@
 
 open System.IO
 
-let readFile () = 
-    File.ReadLines "input.txt"
-    |> Seq.toList
+let parse (line: string) = 
+    line.Split(" -> ")
+    |> Array.toList
+    |> List.map (fun s -> 
+        let parts = s.Split(",")
+        (int <| parts[0], int <| parts[1])
+        )
 
-let task1 = "task 1"
+let readFile () = 
+    File.ReadLines "test-input.txt"
+    |> Seq.toList
+    |> List.map parse
+
+let task1 =
+    let tuples = readFile()
+    let maxHeight = 
+        tuples
+        |> List.concat
+        |> List.map snd
+        |> List.max
+    let maxWidth = 
+        tuples
+        |> List.concat
+        |> List.map fst
+        |> List.max
+    let arr = Array2D.create maxHeight maxWidth
+    (maxWidth, maxHeight)
+
 let task2 = "task 2"
 
 printfn $"Task 1: {task1}"
